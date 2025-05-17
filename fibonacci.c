6247a8e3843dc7c3f4ab7a3a4ba2d4d4311d288d
MikeRaphK <mix.raf.kos@gmail.com>
2025-05-18 02:19:08 +0300

    Add fibonacci.c with recursive, memoization, and dynamic programming implementations

---
+++ fibonacci.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <time.h>
+
+// Recursive Fibonacci function
+int fib_rec(int n) {
+    if (n <= 1) return n;
+    return fib_rec(n - 1) + fib_rec(n - 2);
+}
+
+// Fibonacci function using memoization
+int fib_memo_helper(int n, int memo[]) {
+    if (n <= 1) return n;
+    if (memo[n] != -1) return memo[n];
+    memo[n] = fib_memo_helper(n - 1, memo) + fib_memo_helper(n - 2, memo);
+    return memo[n];
+}
+
+int fib_memo(int n) {
+    int memo[n + 1];
+    for (int i = 0; i <= n; i++) memo[i] = -1;
+    return fib_memo_helper(n, memo);
+}
+
+// Fibonacci function using dynamic programming
+int fib_dp(int n) {
+    if (n <= 1) return n;
+    int dp[n + 1];
+    dp[0] = 0; dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+int main() {
+    int n = 10;
+    clock_t start, end;
+
+    // Test recursive Fibonacci
+    start = clock();
+    printf("Fibonacci (recursive) of %d: %d\n", n, fib_rec(n));
+    end = clock();
+    printf("Time taken by fib_rec: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Test memoization Fibonacci
+    start = clock();
+    printf("Fibonacci (memoization) of %d: %d\n", n, fib_memo(n));
+    end = clock();
+    printf("Time taken by fib_memo: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Test dynamic programming Fibonacci
+    start = clock();
+    printf("Fibonacci (dynamic programming) of %d: %d\n", n, fib_dp(n));
+    end = clock();
+    printf("Time taken by fib_dp: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
